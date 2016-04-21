214a61945ac2da599943a0830946b443c162e795
vedenin <vedenin1980@inbox.ru>
2016-04-22 01:14:46 +0200

    Add GsonHelloWorld

--- helloworlds/3.8-json/gson/src/main/java/gson/GsonHelloWorld.java
+++ helloworlds/3.8-json/gson/src/main/java/gson/GsonHelloWorld.java
@@ -1,5 +1,7 @@
 package gson;
 
+import com.google.gson.Gson;
+
 /**
  *  Gson Hello World
  *
@@ -7,6 +9,59 @@ package gson;
 public class GsonHelloWorld {
 
     public static void main(String[] args) {
+        // init class
+        Place place = new Place();
+        place.setName("World");
 
+        Human human = new Human();
+        human.setMessage("Hi");
+        human.setPlace(place);
+
+        // convert to json
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(human);
+        System.out.println("json " + jsonString); // print "json {"message":"Hi","place":{"name":"World"}}"
+
+        // convert from json
+        Human newHuman = gson.fromJson(jsonString, Human.class);
+        newHuman.say(); // print "Hi , World!"
+    }
+
+    private static class Human {
+        private String message;
+        private Place place;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public Place getPlace() {
+            return place;
+        }
+
+        public void setPlace(Place place) {
+            this.place = place;
+        }
+
+        public void say() {
+            System.out.println();
+            System.out.println(getMessage() + " , " + getPlace().getName() + "!");
+        }
+    }
+
+    private static class Place {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
