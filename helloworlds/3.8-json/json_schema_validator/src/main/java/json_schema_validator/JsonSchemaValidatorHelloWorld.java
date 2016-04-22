1e0c9ac585565bc0446c960d8ff40604c470e355
vedenin <vedenin1980@inbox.ru>
2016-04-22 08:22:26 +0200

    Add JsonSchemaValidator HelloWorld

--- helloworlds/3.8-json/json_schema_validator/src/main/java/json_schema_validator/JsonSchemaValidatorHelloWorld.java
+++ helloworlds/3.8-json/json_schema_validator/src/main/java/json_schema_validator/JsonSchemaValidatorHelloWorld.java
@@ -1,12 +1,40 @@
 package json_schema_validator;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.examples.Utils;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+
+import java.io.IOException;
+
 /**
- *  Json Schema Validator Hello World
- *
-  */
+ * Json Schema Validator Hello World
+ */
 public class JsonSchemaValidatorHelloWorld {
 
-    public static void main(String[] args) {
 
+    public static void main(final String... args)
+            throws IOException, ProcessingException {
+        final JsonNode fstabSchema = Utils.loadResource("/fstab.json");
+        final JsonNode good = Utils.loadResource("/fstab-good.json");
+        final JsonNode bad = Utils.loadResource("/fstab-bad.json");
+        final JsonNode bad2 = Utils.loadResource("/fstab-bad2.json");
+
+        final JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+
+        final JsonSchema schema = factory.getJsonSchema(fstabSchema);
+
+        ProcessingReport report;
+
+        report = schema.validate(good);
+        System.out.println(report);
+
+        report = schema.validate(bad);
+        System.out.println(report);
+
+        report = schema.validate(bad2);
+        System.out.println(report);
     }
 }
