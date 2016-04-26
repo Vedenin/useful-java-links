eb3a05107336aea5bf0f9aede8e03195c263e258
vvedenin <vvedenin@luxoft.com>
2016-04-26 14:07:00 +0200

    Fix templates HelloWorld

---
+++ helloworlds/9.9-template/library_name/src/main/java/library_package/advanced/AdvancedTemplate.java
@@ -0,0 +1,12 @@
+package library_package.advanced;
+
+/**
+ *  Template Hello World examples for this module (more complex then TemplateHelloWorld)
+ *
+ */
+public class AdvancedTemplate {
+
+    public static void main(String[] args) {
+        // HelloWorld example for this library or framework
+    }
+}
